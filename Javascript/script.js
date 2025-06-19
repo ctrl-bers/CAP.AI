73201d1dded8b66e07299733acdac46f24b42cda
Mark <93643165+ctrl-bers@users.noreply.github.com>
2025-06-19 19:00:12 +0800

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -40,7 +40,7 @@ let cooldownUntil = loadCooldown();
 function checkCooldownOnLoad() {
   if (cooldownUntil) {
     const now = Date.now();
-    if (now < parseInt(cooldownUntil, 10)) {
+    if (now < parseInt(cooldownUntil, 1)) {
       showModal();
       generateBtn.disabled = true;
     } else {
